8059cf54a1fb7e1d5426eba5fa8bb5fa0dad602f
sacreutz <sacreutz@gmail.com>
2018-12-17 16:47:53 -0600

    first commit

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  projects: [
+    {id: '1', title: 'Project', content: 'This is project content'},
+    {id: '2', title: 'Project 2', content: 'This is also content'} 
+  ]
+}
+
+const projectReducer = (state = initState, action) => {
+  return state
+}
+
+export default projectReducer
